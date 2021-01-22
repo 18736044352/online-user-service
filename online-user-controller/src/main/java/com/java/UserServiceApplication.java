c1a227a26fb023793656748efd2dd6e955453371
xiaoyu <15576868@qq.com>
2021-01-22 16:33:03 +0800

    初始化

---
+++ online-user-controller/src/main/java/com/java/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package com.java;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+//@MapperScan(basePackages = {"com.java.provider.dao.mappers.*"})//mapper接口自动扫码
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
